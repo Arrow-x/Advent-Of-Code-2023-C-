7a5a0b00c44788e77af4b7e9190578408865bb33
Arrow-x <Houssem.kun26@gmail.com>
2023-12-06 15:28:01 +0100

    day6 is.. fast?

---
+++ day6-part1/Program.cs
@@ -0,0 +1,22 @@
+﻿string data = Directory.GetCurrentDirectory() + @"/data/input";
+if (File.Exists(data) == false) return;
+
+var lines = File.ReadLines(data).ToArray<string>();
+var splitOptions = StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries;
+var raceTimes = lines[0].Split(":", splitOptions)[1].Split(" ", splitOptions).Select(x => int.Parse(x.Trim())).ToArray<int>();
+var recordDistances = lines[1].Split(":", splitOptions)[1].Split(" ", splitOptions).Select(x => int.Parse(x.Trim())).ToArray<int>();
+
+int records = 1;
+for (int race = 0; race < raceTimes.Length; race++)
+{
+    int raceRecord = 0;
+    for (int time = 0; time <= raceTimes[race]; time++)
+    {
+        if (time * (raceTimes[race] - time) > recordDistances[race])
+        {
+            raceRecord += 1;
+        }
+    }
+    records *= raceRecord;
+}
+Console.WriteLine(records);
---
+++ day6-part2/Program.cs
@@ -0,0 +1,16 @@
+﻿string data = Directory.GetCurrentDirectory() + @"/data/input";
+if (File.Exists(data) == false) return;
+
+var lines = File.ReadLines(data).ToArray<string>();
+var splitOptions = StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries;
+var raceTimes = ulong.Parse(lines[0].Split(":", splitOptions)[1].Replace(" ", string.Empty));
+var recordDistances = ulong.Parse(lines[1].Split(":", splitOptions)[1].Replace(" ", string.Empty));
+ulong raceRecord = 0;
+for (ulong time = 0; time <= raceTimes; time++)
+{
+    if (time * (raceTimes - time) > recordDistances)
+    {
+        raceRecord += 1;
+    }
+}
+Console.WriteLine(raceRecord);
